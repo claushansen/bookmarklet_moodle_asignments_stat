60f9995cf9c7cf9c2c91f8393affebcc20e88724
Claus Hansen <claus@multimedion.dk>
2020-11-09 06:24:57 +0100

    Added chart

--- opgavestat-med-filter.js
+++ opgavestat-med-filter.js
@@ -1,13 +1,43 @@
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://www.gstatic.com/charts/loader.js";
+$("head").append(s);
+setTimeout(function () {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+}, 1000);
+
+
 
 var student = $('.page-header-headings h2').text();
 var opgaveresult = [];
 var alleOpgaver = $('.submissionstatustable');
-var antalOpgaver,antalAfleverede,afleveringsprocent,filter;
+var antalOpgaver, antalAfleverede, afleveringsprocent, filter;
+
+
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Opgaver', 'Afleverede'],
+        ['Afleveret', antalAfleverede],
+        ['Ikke Afleveret', antalOpgaver - antalAfleverede]
+    ]);
+
+    var options = {
+        title: 'Afleveringer',
+        backgroundColor: '#f2f2f2'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
 
 function makeInfo() {
     getData();
     var filtertekst = filter !== '' ? ' filtrerede' : '';
-    var infoText = '<h3>' + student + ' har afleveret ' + antalAfleverede + ' ud af ' + antalOpgaver + filtertekst + ' opgaver.</h3>';
+    var infoText = '<h4>' + student + ' har afleveret ' + antalAfleverede + ' ud af ' + antalOpgaver + filtertekst + ' opgaver.</h4>';
     infoText += '<h4>Afleveringsprocenten er derfor på ' + afleveringsprocent + '%.</h4>';
     infoText += '<b>Opgaver udført:</b><br>';
     infoText += '<ul>';
@@ -22,15 +52,16 @@ function makeInfo() {
 
 }
 function makeInfoBox() {
-    
+
     var infodiv = $('<div id="opgaveinfo" class="col-12"><h4 class="pull-left">Opgavestatistik</h4><button id="filterbtn" class="btn btn-dark pull-right">Filter</button><button id="exportbtn" style="margin:0 10px;" class="btn btn-dark pull-right" title="Eksporter som CSV fil.">Export</button><p style="clear:both;">Her kan du få en overskuelig oversigt over, hvilke opgaver eleven har afleveret.</p></div>');
     var infotools = $('<div id="infotools" style="display:none; background-color:#fff; margin-bottom:20px; padding:20px;" class="col-12"><p>Brug filteret til at filtrerer de afleverede opgaver.</p><input type=text id="filter" placeholder="filtreringstekst" class="col-12" style="height: 50px; font-size: 20px; border: 2px solid; border-radius:5px; text-transform:uppercase"></div>');
-    
-    var inforesult = $('<div id="opgaveresult" class="col-12"></div>');
 
-    
+    var inforesult = $('<div id="opgaveresult" class="col-6 pull-left"></div><div id="piechart" class="col-6 pull-right" style="width: 500px; height: 500px;"></div><div style="clear:both;"></div>');
+
+
+
     infodiv.append(infotools);
-    
+
     infodiv.append(inforesult);
     infodiv.css({ 'background-color': '#f2f2f2', 'padding': '20px', 'margin-bottom': '10px' });
 
@@ -42,7 +73,7 @@ function makeInfoBox() {
 function getData() {
     opgaveresult = [];
     filter = $('#filter').val().toUpperCase();
-    
+
     if (filter == '') {
         antalOpgaver = alleOpgaver.length;
         antalAfleverede = $('.submissionstatussubmitted').length;
@@ -50,31 +81,31 @@ function getData() {
         antalOpgaver = 0;
         antalAfleverede = 0;
     }
-        $('.submissionstatustable').parent('ul').prev('h4').each(function (index) {
-            let obj = {};
-            obj.titel = $(this).text().toUpperCase();
-            obj.titel = obj.titel.replace('OPGAVE:','');
-            let findSubmitted = $(alleOpgaver[index]).find('.submissionstatussubmitted');
-            if (findSubmitted.length < 1) {
-                obj.afleveret = 'Nej';
-            } else {
-                obj.afleveret = 'Ja';
-            }
-            if (filter == '') {
-                opgaveresult.push(obj);
-            }
-            else if (filter !== '' && obj.titel.includes(filter)) {
-                opgaveresult.push(obj);
-                antalOpgaver ++;
-                if (obj.afleveret == 'Ja') {
-                    antalAfleverede ++;
-                }
-            }
-
+    $('.submissionstatustable').parent('ul').prev('h4').each(function (index) {
+        let obj = {};
+        obj.titel = $(this).text().toUpperCase();
+        obj.titel = obj.titel.replace('OPGAVE:', '');
+        let findSubmitted = $(alleOpgaver[index]).find('.submissionstatussubmitted');
+        if (findSubmitted.length < 1) {
+            obj.afleveret = 'Nej';
+        } else {
+            obj.afleveret = 'Ja';
         }
-        ); 
-        afleveringsprocent = Math.floor((antalAfleverede / antalOpgaver) * 100);
-        //console.log(JSON.stringify(opgaveresult));
+        if (filter == '') {
+            opgaveresult.push(obj);
+        }
+        else if (filter !== '' && obj.titel.includes(filter)) {
+            opgaveresult.push(obj);
+            antalOpgaver++;
+            if (obj.afleveret == 'Ja') {
+                antalAfleverede++;
+            }
+        }
+
+    }
+    );
+    afleveringsprocent = Math.floor((antalAfleverede / antalOpgaver) * 100);
+    //console.log(JSON.stringify(opgaveresult));
 }
 
 function exportToCsv(filename, rows) {
@@ -118,19 +149,19 @@ function exportToCsv(filename, rows) {
     }
 }
 
-function Export(){
+function Export() {
 
     let csvArray = [];
     csvArray.push(["Opgave", "Afleveret"]);
     console.log(opgaveresult.length);
-    for(i=0; opgaveresult.length>i;i++){
+    for (i = 0; opgaveresult.length > i; i++) {
         console.log(i);
         let titel = opgaveresult[i].titel;
         let afleveret = opgaveresult[i].afleveret;
-        let resultArray = [titel,afleveret];
+        let resultArray = [titel, afleveret];
         csvArray.push(resultArray);
     }
-    exportToCsv('Opgavestatistik-'+student+'.csv',csvArray );
+    exportToCsv('Opgavestatistik-' + student + '.csv', csvArray);
 
 }
 makeInfoBox();
@@ -150,5 +181,6 @@ $('#exportbtn').click(function (e) {
 $('#filter').keyup(function (e) {
     e.preventDefault();
     makeInfo();
+    drawChart();
 
 });
